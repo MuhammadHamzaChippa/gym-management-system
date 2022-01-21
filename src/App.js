4b330c0bba5bb706781759d8e3ceee1a2cfe9305
mhamzachippa <mhamzachippa@gmail.com>
2022-01-22 01:08:57 +0500

    Adding Main UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {Grid , Button , Box , List , ListItem } from '@mui/material'; 
+import {FaUserPlus  , FaRegListAlt , FaCashRegister,FaHome  } from "react-icons/fa";
+import { styled } from "@mui/material/styles";
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    maxWidth: 300 ,
+    margin: 'auto' 
+  },
+  root: {
+    width: '100%' , 
+    backgroundColor: "black"
+  },
+  center: {
+    margin: 'auto' ,
+    backgroundColor: "#c46210", 
+    paddingTop: 3 , 
+    paddingBottom: 3   
+  }
+
+}))
+
+const CustomButton = styled(Button)`
+  color: black ;
+  text-transform: none ; 
+  background-color: #c46210  ;
+  :hover {
+    background-color: #c46210; 
+    color: white ; 
+  } 
+`  
 
 function App() {
+  const classes = useStyles() ; 
   return (
+    <>
+    <Grid className={classes.center} container spacing={1}>
+     <Grid item xs={3}>   
+      <CustomButton  startIcon={<FaHome />} fullWidth>Home</CustomButton>
+     </Grid>
+     <Grid item xs={3}>   
+      <CustomButton  startIcon={<FaUserPlus />} fullWidth>Register Member</CustomButton>
+     </Grid>
+     <Grid item xs={3}>
+      <CustomButton  startIcon={<FaRegListAlt />} fullWidth>Member's List</CustomButton>
+     </Grid>
+     <Grid item xs={3}>
+      <CustomButton  startIcon={<FaCashRegister />} fullWidth>Fee Reminder</CustomButton>
+     </Grid>
+    </Grid>   
+
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box className={classes.paper}>
+          <List className={classes.root}>
+            <ListItem divider>
+              <CustomButton variant="contained" fullWidth startIcon={<FaUserPlus />}>Register Member</CustomButton>
+            </ListItem>
+            <ListItem divider>
+              <CustomButton variant="contained" fullWidth startIcon={<FaRegListAlt />}>Member's List</CustomButton>
+            </ListItem>
+            <ListItem divider>
+              <CustomButton variant="contained" fullWidth startIcon={<FaCashRegister />}>Fee Reminder</CustomButton>
+            </ListItem>
+          </List>
+        </Box>
+        
+        
+        
+        
       </header>
     </div>
+
+    </>
   );
 }
 
---
+++ src/pages/RegisterMember.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RegisterMember() {
+  return (
+    <div>
+        Register Member
+    </div>
+  ) 
+  
+}
+
+export default RegisterMember;
